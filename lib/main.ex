a94a14bb0caa560ec53eac3dc9f321e64c70e0de
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-05-01 17:43:39 +0800

    codecrafters submit [skip ci]

--- lib/main.ex
+++ lib/main.ex
@@ -1,10 +1,24 @@
 defmodule CLI do
+  @commands []
   def main(_args) do
     IO.write("$ ")
+
+    listen()
   end
 
   defp listen do
     input = IO.gets("") |> String.trim()
 
+    case input do
+      :eof -> IO.puts("bye")
+      cmd ->
+        if cmd in @commands do
+          IO.puts("found")
+        else
+          IO.inspect("#{cmd}: not found")
+        end
+    end
+
+    listen()
   end
 end
