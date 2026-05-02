4a044b8969896cc0f17fea12bc5d5ecf260a26f4
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-05-02 20:58:49 +0800

    codecrafters submit [skip ci]

--- lib/commands/execute.ex
+++ lib/commands/execute.ex
@@ -1,7 +1,16 @@
 defmodule Commands.Execute do
-  @bahaviour Commands.Command
+  @behaviour Commands.Command
+  def execute([path | input]) do
+    port = Port.open({:spawn_executable, path}, [
+      :binary,
+      :exit_status,
+      :use_stdio,
+      args: input
+    ])
 
-  def execute(args) do
-    IO.inspect(args, label: "Execute")
+    receive do
+      {^port, {:data, data}} -> IO.puts("executed with #{inspect(data)}")
+      {^port, {:exit_status, code}} -> IO.puts("finished")
+    end
   end
 end
