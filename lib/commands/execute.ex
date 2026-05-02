30c0d24ccc785d45e101710165516541c3d42e64
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-05-02 21:25:49 +0800

    codecrafters submit [skip ci]

--- lib/commands/execute.ex
+++ lib/commands/execute.ex
@@ -1,19 +1,19 @@
 defmodule Commands.Execute do
   @behaviour Commands.Command
 
-  def execute([path | [input]]) do
+  def execute([path, args]) do
     port =
       Port.open({:spawn_executable, path}, [
         :binary,
         :exit_status,
         :use_stdio,
-        args: input
+        args: args
       ])
 
     loop(port)
-
-    IO.puts("Program was passed #{Enum.count(input) + 1} args (including program name).")
   end
+
+  def execute(_args), do: :error
 
   defp loop(port) do
     receive do
@@ -23,16 +23,6 @@ defmodule Commands.Execute do
 
       {^port, {:exit_status, _code}} ->
         :ok
-    end
-  end
-
-  def execute(_args), do: :error
-
-  def encode_exe_output(data) do
-    [data | inputs] = String.split(data, "\n")
-
-    for input <- inputs do
-      IO.puts(input)
     end
   end
 end
