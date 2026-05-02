55650d8ea4d8f35bb7878f46f608f1a69f57df8c
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-05-02 19:23:13 +0800

    codecrafters submit [skip ci]

--- lib/commands/type.ex
+++ lib/commands/type.ex
@@ -14,7 +14,20 @@ defmodule Commands.Type do
       true ->
         IO.puts("#{command} is a shell builtin")
       false ->
+        System.get_env("PATH")
+        |> String.split(":")
+        |> Enum.map(&Path.join(&1, command))
+        |> Enum.find(&executable?/1)
         IO.puts("#{command}: not found")
+    end
+  end
+
+  defp executable?(path) do
+    case File.stat(path) do
+      {:ok, %File.Stat{mode: mode}} ->
+        (mode & 0o111) != 0
+      {:error, _} ->
+        false
     end
   end
 end
