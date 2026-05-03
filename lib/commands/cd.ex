8cd20c16cfe34b203a28c18c9049fa5ae525c3ff
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-05-03 23:42:42 +0800

    codecrafters submit [skip ci]

--- lib/commands/cd.ex
+++ lib/commands/cd.ex
@@ -2,6 +2,14 @@ defmodule Commands.CD do
   @behaviour Commands.Command
 
   def execute(args) do
+    if Path.relative?(args) do
+      cd_working_dir(args)
+    else
+      IO.puts("error: #{args}")
+    end
+  end
+
+  defp cd_working_dir(args) do
     case File.exists?(args) do
       true ->
         File.cd(args)
