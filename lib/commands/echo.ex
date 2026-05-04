9f1ad2bbfceb18d0e162287e72fc32c48b43769d
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-05-04 20:49:30 +0800

    codecrafters submit [skip ci]

--- lib/commands/echo.ex
+++ lib/commands/echo.ex
@@ -2,9 +2,6 @@ defmodule Commands.Echo do
   @behaviour Commands.Command
 
   def execute(args) do
-    IO.puts(
-      ""
-    )
     if String.at(args, 0) == "'" and String.at(args, -1) == "'" do
       IO.inspect(Enum.join(String.slice(args, 1..-2), " "))
     end
