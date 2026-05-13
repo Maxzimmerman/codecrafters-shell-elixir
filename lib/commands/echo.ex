97a80dbc23c277b209b16f67d8c54033ffcdcd80
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-05-14 01:05:05 +0800

    codecrafters submit [skip ci]

--- lib/commands/echo.ex
+++ lib/commands/echo.ex
@@ -2,7 +2,12 @@ defmodule Commands.Echo do
   @behaviour Commands.Command
 
   def execute(args) do
-    IO.inspect(args, label: "TEST")
-    IO.puts(Enum.join(args, " "))
+    case Enum.split_while(args, fn a -> a not in [">", "1>"] end) do
+      {words, [_op, file | _]} ->
+        File.write!(file, Enum.join(words, " ") <> "\n")
+
+      {words, []} ->
+        IO.puts(Enum.join(words, " "))
+    end
   end
 end
