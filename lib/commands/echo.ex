d5891548043ce05f6e485d0778dbe5d337638b7f
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-05-20 00:12:17 +0800

    codecrafters submit [skip ci]

--- lib/commands/echo.ex
+++ lib/commands/echo.ex
@@ -2,15 +2,6 @@ defmodule Commands.Echo do
   @behaviour Commands.Command
 
   def execute(args) do
-    case Enum.split_while(args, fn a -> a not in [">", "1>", ">>", "1>>"] end) do
-      {words, [op, file | _]} when op in [">>", "1>>"] ->
-        File.write!(file, Enum.join(words, " ") <> "\n", [:append])
-
-      {words, [op, file | _]} when op in [">", "1>"] ->
-        File.write!(file, Enum.join(words, " ") <> "\n")
-
-      {words, []} ->
-        IO.puts(Enum.join(words, " "))
-    end
+    IO.puts(Enum.join(args, " "))
   end
 end
--- lib/commands/execute.ex
+++ lib/commands/execute.ex
@@ -20,35 +20,7 @@ defmodule Commands.Execute do
     loop(port)
   end
 
-  def execute([command_path, [flag, read_file, op, output_file]])
-      when op in [">>", "1>>"] do
-    port =
-      Port.open({:spawn_executable, command_path}, [
-        :binary,
-        :exit_status,
-        :use_stdio,
-        arg0: Path.basename(command_path),
-        args: [flag, read_file]
-      ])
-
-    loop_append_stdout(port, output_file, [])
-  end
-
-  def execute([command_path, [flag, read_file, op, output_file]] = _input)
-      when op in [">", "1>"] do
-    port =
-      Port.open({:spawn_executable, command_path}, [
-        :binary,
-        :exit_status,
-        :use_stdio,
-        arg0: Path.basename(command_path),
-        args: [flag, read_file]
-      ])
-
-    loop(port, output_file, [])
-  end
-
-  def execute([path, args] = _input) do
+  def execute([path, args]) do
     port =
       Port.open({:spawn_executable, path}, [
         :binary,
@@ -62,32 +34,6 @@ defmodule Commands.Execute do
   end
 
   def execute(_args), do: :error
-
-  defp loop(port, output_file, output_data) do
-    receive do
-      {^port, {:data, data}} ->
-        loop(port, output_file, [output_data | data])
-
-      {^port, {:exit_status, _code}} ->
-        {:ok, file} = File.open(output_file, [:write])
-        IO.binwrite(file, output_data)
-        File.close(file)
-        :ok
-    end
-  end
-
-  def loop_append_stdout(port, output_file, output_data) do
-    receive do
-      {^port, {:data, data}} ->
-        loop_append_stdout(port, output_file, [output_data | data])
-
-      {^port, {:exit_status, _code}} ->
-        {:ok, file} = File.open(output_file, [:append])
-        IO.binwrite(file, output_data)
-        File.close(file)
-        :ok
-    end
-  end
 
   defp loop(port) do
     receive do
