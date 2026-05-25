2dad5d92b0bae48e6bea30d410802d2717ff3efb
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-05-26 00:39:15 +0800

    codecrafters submit [skip ci]

--- lib/main.ex
+++ lib/main.ex
@@ -93,7 +93,6 @@ defmodule CLI do
 
       found_matches when length(found_matches) > 1 and count == 1 ->
         IO.write(Enum.join(found_matches, "  "))
-        IO.puts(buf)
 
       _ ->
         IO.puts("\x07")
