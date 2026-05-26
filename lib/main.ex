2ba06cc239e0b964db6602ed7d969b1803626857
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-05-26 21:52:48 +0800

    codecrafters submit [skip ci]

--- lib/main.ex
+++ lib/main.ex
@@ -89,8 +89,6 @@ defmodule CLI do
         "."
       end
 
-    IO.inspect(dir_to_look_for, label: "TEST")
-
     file_matches =
       Enum.filter(
         Commands.list_files_in_dir(dir_to_look_for),
