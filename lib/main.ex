d510dcbcd76be779b9dcaf61097e75eb24b55121
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-06-09 00:43:14 +0800

    codecrafters submit [skip ci]

--- lib/main.ex
+++ lib/main.ex
@@ -93,6 +93,7 @@ defmodule CLI do
       found_matches when length(found_matches) > 1 and count == 0 ->
         [match | _] = found_matches
         suffix = String.replace_prefix(match <> " ", buf, "")
+        IO.write("\n")
         match <> " "
 
       found_matches when length(found_matches) > 1 and count >= 1 ->
