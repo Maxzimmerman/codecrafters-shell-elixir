86eb9a52a8449206f9dfef24a1a819c58898e792
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-06-14 21:57:08 +0800

    codecrafters submit [skip ci]

--- lib/caches/registered_completion_scripts_cache.ex
+++ lib/caches/registered_completion_scripts_cache.ex
@@ -20,11 +20,20 @@ defmodule RegisteredCompletionScriptsCache do
     {:noreply, Map.put(state, name, path)}
   end
 
+  @impl true
+  def handle_cast({:get_script, name}, state) do
+    {:noreply, Map.get(state, name)}
+  end
+
   def get_state do
     GenServer.call(__MODULE__, :get_state)
   end
 
   def set_state(name, path) do
     GenServer.cast(__MODULE__, {:add_script, name, path})
+  end
+
+  def get_with_name(name) do
+    GenServer.cast(__MODULE__, {:get_script, name})
   end
 end
