ef06e362cbf0cb924b40bd56595c04444cc8851b
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-06-23 00:16:02 +0800

    codecrafters submit [skip ci]

--- lib/main.ex
+++ lib/main.ex
@@ -260,7 +260,7 @@ defmodule CLI do
         :ok
 
       [command, input, "&" | _] ->
-        IO.inspect("[1] #{self()}")
+        IO.puts("[1] #{inspect(self())}")
         Task.async(fn -> dispatch(command, input) end)
 
       [command | input] ->
