8ebd7439d08da8b023b35f26a203cb0a4367b655
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-06-22 23:56:22 +0800

    codecrafters submit [skip ci]

---
+++ lib/caches/jobs_cache.ex
@@ -0,0 +1,11 @@
+defmodule JobsCache do
+  use GenServer
+
+  def start_link(_opts \\ []) do
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  def init(_) do
+    {:ok, %{}}
+  end
+end
