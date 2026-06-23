43aa117636e272b98723b742b278593d4478ee2d
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-06-23 13:03:05 +0800

    codecrafters submit [skip ci]

--- lib/commands/execute.ex
+++ lib/commands/execute.ex
@@ -44,43 +44,39 @@ defmodule Commands.Execute do
 
     sh = System.find_executable("sh")
 
-    spawn(fn ->
-      port =
-        Port.open({:spawn_executable, sh}, [
-          :binary,
-          :exit_status,
-          :use_stdio,
-          arg0: "sh",
-          args: ["-c", cmd_string]
-        ])
+    port =
+      Port.open({:spawn_executable, sh}, [
+        :binary,
+        :exit_status,
+        :use_stdio,
+        arg0: "sh",
+        args: ["-c", cmd_string]
+      ])
 
-      {:os_pid, pid} = :erlang.port_info(port, :os_pid)
+    {:os_pid, pid} = :erlang.port_info(port, :os_pid)
 
-      IO.puts("[1] #{pid}")
+    IO.puts("[1] #{pid}")
 
-      loop(port)
-    end)
+    loop(port)
 
     :ok
   end
 
   def execute([path, args], true) do
-    spawn(fn ->
-      port =
-        Port.open({:spawn_executable, path}, [
-          :binary,
-          :exit_status,
-          :use_stdio,
-          arg0: Path.basename(path),
-          args: args
-        ])
+    port =
+      Port.open({:spawn_executable, path}, [
+        :binary,
+        :exit_status,
+        :use_stdio,
+        arg0: Path.basename(path),
+        args: args
+      ])
 
-      {:os_pid, pid} = :erlang.port_info(port, :os_pid)
+    {:os_pid, pid} = :erlang.port_info(port, :os_pid)
 
-      IO.puts("[1] #{pid}")
+    IO.puts("[1] #{pid}")
 
-      loop(port)
-    end)
+    loop(port)
 
     :ok
   end
