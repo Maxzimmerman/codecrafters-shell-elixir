ab821a5057e72737181bfacb0cfc3031f407232c
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-06-23 12:44:27 +0800

    codecrafters submit [skip ci]

--- lib/commands/execute.ex
+++ lib/commands/execute.ex
@@ -35,6 +35,44 @@ defmodule Commands.Execute do
     loop(port)
   end
 
+  def execute([command_path, args, {stderr_file, mode}], true) when is_binary(stderr_file) do
+    op = if mode == :append, do: " 2>> ", else: " 2> "
+
+    cmd_string =
+      ([command_path | args] |> Enum.map(&shell_escape/1) |> Enum.join(" ")) <>
+        op <> shell_escape(stderr_file)
+
+    sh = System.find_executable("sh")
+
+    port =
+      Port.open({:spawn_executable, sh}, [
+        :binary,
+        :exit_status,
+        :use_stdio,
+        arg0: "sh",
+        args: ["-c", cmd_string]
+      ])
+
+    {:os_pid, pid} = :erlang.port_info(port, :os_pid)
+
+    IO.puts("[1] #{pid}")
+  end
+
+  def execute([path, args], true) do
+    port =
+      Port.open({:spawn_executable, path}, [
+        :binary,
+        :exit_status,
+        :use_stdio,
+        arg0: Path.basename(path),
+        args: args
+      ])
+
+    {:os_pid, pid} = :erlang.port_info(port, :os_pid)
+
+    IO.puts("[1] #{pid}")
+  end
+
   def execute(_args), do: :error
 
   defp loop(port) do
