a918b42305d965d3f854e0fba330103b34a1c9fe
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-06-25 18:11:22 +0800

    codecrafters submit [skip ci]

--- lib/commands/jobs.ex
+++ lib/commands/jobs.ex
@@ -1,33 +1,26 @@
 defmodule Commands.Jobs do
   @behaviour Commands.Command
 
-  alias DataTypes.BackgroundJob
-
   @impl true
-  def execute(args) do
-    jobs =
-      JobsCache.get_all()
+  def execute(_args) do
+    jobs = JobsCache.get_all() |> Enum.sort_by(& &1.job_number)
+    total = length(jobs)
 
-    if length(jobs) == 0 do
-      :ok
-    else
-      [job | _] = jobs
+    jobs
+    |> Enum.with_index()
+    |> Enum.each(fn {job, idx} ->
+      marker =
+        case total - 1 - idx do
+          0 -> "+"
+          1 -> "-"
+          _ -> " "
+        end
 
       command =
         String.split(job.command_str, "/")
         |> Enum.at(-1)
 
-      case Enum.find_index(jobs, &(&1.job_number == job.job_number)) do
-        nil ->
-          :not_found
-
-        idx ->
-          case length(jobs) - 1 - idx do
-            0 -> IO.puts("[#{job.job_number}]+  Running                 #{command}")
-            1 -> IO.puts("[#{job.job_number}]-  Running                 #{command}")
-            _ -> IO.puts("[#{job.job_number}]   Running                 #{command}")
-          end
-      end
-    end
+      IO.puts("[#{job.job_number}]#{marker}  Running                 #{command}")
+    end)
   end
 end
