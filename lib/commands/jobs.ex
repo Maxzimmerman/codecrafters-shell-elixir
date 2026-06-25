59b702fd53c30d849d1a90a3a254a4cefd93f770
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-06-25 18:05:59 +0800

    codecrafters submit [skip ci]

--- lib/commands/jobs.ex
+++ lib/commands/jobs.ex
@@ -17,8 +17,17 @@ defmodule Commands.Jobs do
         String.split(job.command_str, "/")
         |> Enum.at(-1)
 
-      IO.inspect(args)
-      IO.puts("[#{job.job_number}]+  Running                 #{command}")
+      case Enum.find_index(jobs, &(&1.id == job.id)) do
+        nil ->
+          :not_found
+
+        idx ->
+          case length(jobs) - 1 - idx do
+            0 -> IO.puts("[#{job.job_number}]+  Running                 #{command}")
+            1 -> IO.puts("[#{job.job_number}]-  Running                 #{command}")
+            _ -> IO.puts("[#{job.job_number}]   Running                 #{command}")
+          end
+      end
     end
   end
 end
