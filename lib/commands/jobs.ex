1fc6ab4bdd552bbbae8d84c229e840cb085b720f
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-06-25 17:52:53 +0800

    codecrafters submit [skip ci]

--- lib/commands/jobs.ex
+++ lib/commands/jobs.ex
@@ -1,9 +1,15 @@
 defmodule Commands.Jobs do
   @behaviour Commands.Command
 
+  alias DataTypes.BackgroundJob
+
   @impl true
   def execute(_args) do
-    jobs = JobsCache.get_all()
+    jobs =
+      JobsCache.get_all()
+      |> Enum.sort(fn %BackgroundJob{job_number: job_number} ->
+        job_number
+      end)
 
     case length(jobs) do
       1 ->
