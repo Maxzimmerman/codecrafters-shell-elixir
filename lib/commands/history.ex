cbc6d4a2dce32cb5f5e4f26ba97c26555e563823
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-06-27 17:42:33 +0800

    codecrafters submit [skip ci]

--- lib/commands/history.ex
+++ lib/commands/history.ex
@@ -2,6 +2,13 @@ defmodule Commands.History do
   @behaviour Commands.Command
 
   def execute(_) do
-    IO.inspect(HistoryCache.get_all() |> Enum.reverse())
+    HistoryCache.get_all()
+    |> print_history()
+  end
+
+  def print_history(history) do
+    history
+    |> Enum.reverse()
+    |> IO.inspect()
   end
 end
