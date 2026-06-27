161cf55c43637f5c7793ae92eee215f7566b6544
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-06-27 17:50:09 +0800

    codecrafters submit [skip ci]

--- lib/commands/history.ex
+++ lib/commands/history.ex
@@ -1,14 +1,21 @@
 defmodule Commands.History do
   @behaviour Commands.Command
 
+  def execute([number]) do
+    HistoryCache.get_all()
+    |> Enum.reverse()
+    |> Enum.take(number)
+    |> print_history()
+  end
+
   def execute(_) do
     HistoryCache.get_all()
+    |> Enum.reverse()
     |> print_history()
   end
 
   def print_history(history) do
     history
-    |> Enum.reverse()
     |> Enum.map(&Enum.join(&1, " "))
     |> Enum.with_index(fn line, index ->
       IO.puts("#{index} #{line}")
