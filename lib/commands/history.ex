92b8e82067a1954ac827b2d5f0ce1aa71ad79fe3
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-07-11 21:58:24 +0800

    codecrafters submit [skip ci]

--- lib/commands/history.ex
+++ lib/commands/history.ex
@@ -35,19 +35,6 @@ defmodule Commands.History do
     end)
   end
 
-  def print_most_recent(history, count) do
-    lines =
-      history
-      |> Enum.map(&Enum.join(&1, " "))
-      |> Enum.reverse()
-
-    output =
-      lines
-      |> Enum.at(count + 1)
-
-    IO.puts("#{output}")
-  end
-
   defp format_index(index) do
     index |> Integer.to_string() |> String.pad_leading(5)
   end
