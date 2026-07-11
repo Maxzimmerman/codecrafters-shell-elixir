495442698f934e31c6e60a5a81b1564b65f4e8b4
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-07-11 21:49:06 +0800

    codecrafters submit [skip ci]

--- lib/main.ex
+++ lib/main.ex
@@ -81,7 +81,7 @@ defmodule CLI do
       27 ->
         case {read_byte(), read_byte()} do
           {?[, ?A} ->
-            handle_up(buf, arrow_up_count) |> read_line(tab_count, arrow_up_count + 1)
+            handle_up(buf, arrow_up_count + 1) |> read_line(tab_count, arrow_up_count + 1)
 
           _ ->
             read_line(buf, tab_count, arrow_up_count + 1)
