146e58f712d687d22e06c7688148847ad0cc2913
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-08-22 22:20:25 +0800

    codecrafters submit [skip ci]

--- lib/commands/history.ex
+++ lib/commands/history.ex
@@ -52,8 +52,8 @@ defmodule Commands.History do
 
   defp save_multiple_in_cache(list) do
     list
-    |> String.replace("\n", "")
-    |> String.split(" ")
+    |> String.replace("word/word", ~r"/", "\\\\")
+    |> Enum.map(fn item -> String.replace(item, "\n", " ") end)
     |> Enum.each(fn item -> HistoryCache.add_one([item]) end)
   end
 end
