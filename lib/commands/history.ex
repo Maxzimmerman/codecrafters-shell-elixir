566d6078df730ee33ffa3a33c26c71d2f83da534
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-08-22 22:14:43 +0800

    codecrafters submit [skip ci]

--- lib/commands/history.ex
+++ lib/commands/history.ex
@@ -9,6 +9,7 @@ defmodule Commands.History do
   def execute(["-r", file_path]) do
     case File.read(file_path) do
       {:ok, content} ->
+        IO.inspect(content, label: "TESET")
         save_multiple_in_cache(content)
 
       {:error, reason} ->
