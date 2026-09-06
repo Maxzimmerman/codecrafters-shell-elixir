79e9a095963cbf6615bc52e8e06008ea9fd400bf
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-09-06 23:50:11 +1000

    codecrafters submit [skip ci]

--- lib/commands/declare.ex
+++ lib/commands/declare.ex
@@ -1,8 +1,8 @@
 defmodule Commands.Declare do
   @behaviour Commands.Command
 
-  def execute(args) do
-    IO.inspect(args)
+  def execute(["-p", variable_name | _]) do
+    IO.inspect(variable_name)
   end
 
   def execute(args) do
