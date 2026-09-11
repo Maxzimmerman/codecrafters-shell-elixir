58fee2748b7894d98b37f52cf7d44b1f7a58b58d
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-09-11 23:50:14 +0800

    codecrafters submit [skip ci]

--- lib/commands/declare.ex
+++ lib/commands/declare.ex
@@ -7,8 +7,8 @@ defmodule Commands.Declare do
     end
   end
 
-  def execute([key <> "=" <> value]) do
-    IO.inspect(%{key => value})
+  def execute(["=" <> value]) do
+    IO.inspect(%{"test" => value})
   end
 
   def execute(args) do
