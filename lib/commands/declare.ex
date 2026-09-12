d090d803625effe3629bf78f649c5d4297618476
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-09-12 21:11:53 +0800

    codecrafters submit [skip ci]

--- lib/commands/declare.ex
+++ lib/commands/declare.ex
@@ -1,6 +1,8 @@
 defmodule Commands.Declare do
   @behaviour Commands.Command
 
+  @key_regex ~r/^[a-zA-Z_]/
+
   def execute(["-p", variable_name | _]) do
     case VariableCache.get_one(variable_name) do
       :not_found -> IO.puts("declare: #{variable_name}: not found")
@@ -9,7 +11,11 @@ defmodule Commands.Declare do
   end
 
   def execute([input]) do
-    [key, val] = String.split(input, "=")
-    VariableCache.add_one(key, val)
+    with [key, val] <- String.split(input, "="),
+         true <- validate_key(key) do
+      VariableCache.add_one(key, val)
+    end
   end
+
+  defp validate_key(key), do: Regex.match?(@key_regex, key)
 end
